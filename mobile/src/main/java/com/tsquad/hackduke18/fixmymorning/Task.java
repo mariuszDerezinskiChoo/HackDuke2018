71db0c86174f2ea2dbb7545a3b85568354533573
Nishanth Singaraju <43304227+NishanthSingaraju@users.noreply.github.com>
2018-10-13 21:37:26 -0400

    Add files via upload

--- mobile/src/main/java/com/tsquad/hackduke18/fixmymorning/Task.java
+++ mobile/src/main/java/com/tsquad/hackduke18/fixmymorning/Task.java
@@ -1,38 +1,44 @@
-public class Task() {
-        private String name;
-        private float upper;
-        private float lower;
-        private int priority;
-        private String date;
-        private float order;
-       
-        public Task(String Name, float Upper, float Lower, float ORDER, int PRIORITY, STRING DATE ){
-            name = Name;
-            upper= Upper;
-            lower= Lower;
-            order= ORDER;
-            priority= PRIORITY;
-            date= DATE;
-     public String getName( ) {
-      return name;
-   }
-   public float getUpper( ) {
-      return upper;
-   }
-   public float getLower( ) {
-      return name;
-   }
-   public float getOrder( ) {
-      return order;
-   }
+public class Task {
+    private String name;
+    private float upper;
+    private float lower;
+    private int priority;
+    private String date;
+    private float order;
 
-public int getPriority( ) {
-      return priority;
-   }
-public String getDate( ) {
-      return date;
-   }
-
-        }
-
+    public Task(String name, float Upper, float Lower, float ORDER, int PRIORITY, String DATE) {
+        name = name;
+        upper = Upper;
+        lower = Lower;
+        order = ORDER;
+        priority = PRIORITY;
+        date = DATE;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getUpper() {
+        return upper;
+    }
+
+    public float getLower() {
+        return lower;
+    }
+
+    public float getOrder() {
+        return order;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+}
+
+
